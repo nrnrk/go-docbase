37afa0c9bf47042a9e4ef86304c234b38923ae0f
nrnrk <noriki6t@gmail.com>
2021-02-17 22:40:02 +0900

    Support basic post feature

--- client.go
+++ client.go
@@ -12,6 +12,8 @@ import (
 type (
 	Client interface {
 		CreatePost(ctx context.Context, req *CreatePostRequest) (*Post, error)
+		UpdatePost(ctx context.Context, req *UpdatePostRequest) (*Post, error)
+		GetPost(ctx context.Context, id uint) (*Post, error)
 	}
 
 	client struct {
@@ -37,6 +39,43 @@ func (c *client) CreatePost(ctx context.Context, req *CreatePostRequest) (*Post,
 		return nil, fmt.Errorf("Failed to create http reqeust: %w", err)
 	}
 
+	post, err := c.executeAPI(httpReq)
+	if err != nil {
+		return nil, fmt.Errorf("DocBase API error: %w", err)
+	}
+
+	return post, nil
+}
+
+func (c *client) UpdatePost(ctx context.Context, req *UpdatePostRequest) (*Post, error) {
+	httpReq, err := c.createUpdateRequest(ctx, req)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create http reqeust: %w", err)
+	}
+
+	post, err := c.executeAPI(httpReq)
+	if err != nil {
+		return nil, fmt.Errorf("DocBase API error: %w", err)
+	}
+
+	return post, nil
+}
+
+func (c *client) GetPost(ctx context.Context, id uint) (*Post, error) {
+	httpReq, err := c.createGetRequest(ctx, id)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create http reqeust: %w", err)
+	}
+
+	post, err := c.executeAPI(httpReq)
+	if err != nil {
+		return nil, fmt.Errorf("DocBase API error: %w", err)
+	}
+
+	return post, nil
+}
+
+func (c *client) executeAPI(httpReq *http.Request) (*Post, error) {
 	res, err := c.httpClient.Do(httpReq)
 	if err != nil {
 		return nil, fmt.Errorf("Failed to send http reqeust: %w", err)
@@ -82,5 +121,41 @@ func (c *client) createPostRequest(ctx context.Context, req *CreatePostRequest) 
 
 	httpReq.Header.Add(`X-DocBaseToken`, c.token)
 	httpReq.Header.Add(`Content-Type`, `application/json`)
+	return httpReq, nil
+}
+
+func (c *client) createUpdateRequest(ctx context.Context, req *UpdatePostRequest) (*http.Request, error) {
+	jsonReq, err := json.Marshal(req)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to marshal request to json: %w", err)
+	}
+
+	httpReq, err := http.NewRequestWithContext(
+		ctx,
+		`PATCH`,
+		fmt.Sprintf("https://api.docbase.io/teams/%s/posts", c.team),
+		bytes.NewReader(jsonReq),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create request: %w", err)
+	}
+
+	httpReq.Header.Add(`X-DocBaseToken`, c.token)
+	httpReq.Header.Add(`Content-Type`, `application/json`)
+	return httpReq, nil
+}
+
+func (c *client) createGetRequest(ctx context.Context, id uint) (*http.Request, error) {
+	httpReq, err := http.NewRequestWithContext(
+		ctx,
+		`GET`,
+		fmt.Sprintf("https://api.docbase.io/teams/%s/posts/%d", c.team, id),
+		nil,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create request: %w", err)
+	}
+
+	httpReq.Header.Add(`X-DocBaseToken`, c.token)
 	return httpReq, nil
 }
